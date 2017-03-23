eaa93f7413c85beb72de65f94e47cd111c901bf2
Cristian Marculescu <marculescucristian94@gmail.com>
2017-03-23 12:33:14 +0000

    Added fingerprint scanner code

---
+++ fingerpi-master/licenta.py
@@ -0,0 +1,110 @@
+import fingerpi as fp
+import time
+
+DELAY_TIME = 0.1
+BAUD_RATE = 115200
+
+def CycleUntilPressed(f):
+    presence_count = 0
+    while presence_count < 5:
+        response = f.IsPressFinger()
+        if response[0]['Parameter'] == 0:
+            presence_count += 1
+        time.sleep(DELAY_TIME)
+
+def CycleUntilNotPressed(f):
+    presence_count = 10
+    while not presence_count <= 0:
+        response = f.IsPressFinger()
+        if not response[0]['Parameter'] == 0:
+            presence_count -= 1
+        time.sleep(DELAY_TIME)
+
+def CheckFingerPress(f):
+    f.CmosLed(True)
+    time.sleep(DELAY_TIME)
+    print 'Checking finger presence... Put your finger in the scanner...'
+    finger_pressed = 0
+    while True:
+        response = f.IsPressFinger()
+        if response[0]['Parameter'] == 0:
+            print 'Finger is pressed!'
+        else:
+            print 'Finger is not pressed!'
+        time.sleep(DELAY_TIME)
+
+def capture(f, phase):
+    print 'Place your fingerprint now and please keep it pressed'
+    f.CmosLed(True)
+    time.sleep(DELAY_TIME)
+    CycleUntilPressed(f)
+    if phase == 1:
+        print 'Starting 1st phase of enrollment, please do not lift your finger...'
+        print f.CaptureFinger(True)
+        time.sleep(DELAY_TIME)
+        print f.Enroll1()
+        time.sleep(DELAY_TIME)
+        print '1st phase complete. Please lift your finger for a moment...'
+        f.CmosLed(False)
+        time.sleep(DELAY_TIME)
+        CycleUntilNotPressed(f)
+    elif phase == 2:
+        print 'Starting 2nd phase of enrollment, please do not lift your finger...'
+        print f.CaptureFinger(True)
+        time.sleep(DELAY_TIME)
+        print f.Enroll2()
+        time.sleep(DELAY_TIME)
+        print '2nd phase complete. Please lift your finger for a moment...'
+        f.CmosLed(False)
+        time.sleep(DELAY_TIME)
+        CycleUntilNotPressed(f)
+    elif phase == 3:
+        print 'Starting final phase of enrollment, please do not lift your finger...'
+        print f.CaptureFinger(True)
+        time.sleep(DELAY_TIME)
+        print f.Enroll3()
+        time.sleep(DELAY_TIME)
+        f.CmosLed(False)
+        time.sleep(DELAY_TIME)
+                
+## Doesn't really work...
+def EnrollFingerPrint(f):
+##  100: left thumb 101: right thumb
+    id = 50
+    print 'Starting fingerprint enroll process...'
+    print f.EnrollStart(id)
+    time.sleep(DELAY_TIME)
+    for phase in xrange(1, 4):
+        capture(f, phase)
+    print 'Fingerprint enroll completed, registered at id: ' + str(id)
+
+def GetFingerID(f):
+    print 'Starting fingerprint matching, please put your finger in the scanner...'
+    f.CmosLed(True)
+    time.sleep(DELAY_TIME)
+    CycleUntilPressed(f)
+    f.CaptureFinger(False)
+    time.sleep(DELAY_TIME)
+    f.CmosLed(False)
+    time.sleep(DELAY_TIME)
+    response = f.Identify()
+##  Modified base.py line 107 to make this work    
+    if response[0]['ACK']:
+        print 'Your fingerprint matched with the id: ' + str(response[0]['Parameter'])
+    else:
+        print 'Your fingerprint did not match'
+
+if __name__ == '__main__':
+    f = fp.FingerPi(port = '/dev/ttyAMA0')
+    print 'Opening connection...'
+    f.Open(extra_info = False, check_baudrate = False)
+    time.sleep(DELAY_TIME)
+    print 'Setting baudrate to: ' + str(BAUD_RATE)
+    f.ChangeBaudrate(BAUD_RATE)
+    time.sleep(DELAY_TIME)
+##    EnrollFingerPrint(f)
+##    print f.GetEnrollCount()
+    GetFingerID(f)
+##    CheckFingerPress(f)
+    print 'Closing connection...'
+    f.Close()
---
+++ pyGT511C3-master/licenta.py
@@ -0,0 +1,149 @@
+import FPS, sys
+
+def checkFingerPress():
+    fps.SetLED(True) # Turns ON the CMOS LED
+    FPS.delay(0.1) # wait 1 second
+    print 'Checking finger presence... Put your finger in the scanner...'
+    presenceCount = 0
+    while True:
+        if fps.IsPressFinger():
+            presenceCount += 1
+            if presenceCount == 3:
+                print 'Your finger is in the scanner!'
+                FPS.delay(0.1)
+                fps.SetLED(False)
+                FPS.delay(0.5)
+                print 'Enrolled fingerprints: ' + str(fps.GetEnrollCount())
+                FPS.delay(0.5)
+                fps.Close()
+                return
+        FPS.delay(0.1)
+
+def cycleUntilPressed(fps, delayTimer = 0.5):
+    presenceCount = 0
+    while True:
+        if fps.IsPressFinger():
+            presenceCount += 1
+            if presenceCount == 3:
+                FPS.delay(delayTimer)
+                return
+        FPS.delay(delayTimer)
+
+def cycleUntilNotPressed(fps, delayTimer = 0.5):
+    recentPresses = 5
+    while True:
+        if fps.IsPressFinger():
+            continue
+        else:
+            recentPresses -= 1
+            if recentPresses == 0:
+                FPS.delay(delayTimer)
+                return
+        FPS.delay(delayTimer)
+
+def capture(fps, phase, delayTimer = 0.5):
+    print 'Place your fingerprint now and please keep it pressed'
+    fps.SetLED(True)
+    FPS.delay(delayTimer)
+    cycleUntilPressed(fps)
+    if phase == 1:
+        print 'Starting 1st phase of enrollment, please do not lift your finger...'
+        if fps.CaptureFinger() == False:
+            print 'Something went wrong while capturing your fingerprint'
+            return False
+        else:
+            FPS.delay(delayTimer)
+            if fps.Enroll1() != 0:
+                print 'Some error occured at Enroll1'
+                return False
+            FPS.delay(delayTimer)
+            print '1st phase complete. Please lift your finger for a moment...'
+            fps.SetLED(False)
+            FPS.delay(delayTimer)
+            cycleUntilNotPressed(fps)
+            return True
+    elif phase == 2:
+        print 'Starting 2nd phase of enrollment, please do not lift your finger...'
+        if fps.CaptureFinger() == False:
+            print 'Something went wrong while capturing your fingerprint'
+            return False
+        else:
+            FPS.delay(delayTimer)
+            if fps.Enroll2() != 0:
+                print 'Some error occured at Enroll2'
+                return False
+            FPS.delay(delayTimer)
+            print '2nd phase complete. Please lift your finger for a moment...'
+            fps.SetLED(False)
+            FPS.delay(delayTimer)
+            cycleUntilNotPressed(fps)
+            return True
+    elif phase == 3:
+        print 'Starting final phase of enrollment, please do not lift your finger...'
+        if fps.CaptureFinger() == False:
+            print 'Something went wrong while capturing your fingerprint'
+            return False
+        else:
+            FPS.delay(delayTimer)
+            if fps.Enroll3() != 0:
+                print 'Some error occured at Enroll3'
+                return False
+            FPS.delay(delayTimer)
+            print 'Final phase complete. Fingerprint has been registered!'
+            fps.SetLED(False)
+            FPS.delay(delayTimer)
+            return True
+
+def enrollFingerprint(fps, delayTimer = 0.5):
+    print 'Starting fingerprint enroll process...'
+##  fingerprint id 69, left middle finger
+##  fingerprint id 100, left big finger
+    if fps.EnrollStart(101) != 0:
+        print 'Some error occured at EnrollStart'
+        return
+    FPS.delay(delayTimer)
+    for i in xrange(1, 4):
+        if not capture(fps, i):
+            return
+        
+def fingerprintID(fps, delayTimer = 0.5):
+    print 'Starting fingerprint mathing...'
+    print 'Place your fingerprint now and please keep it pressed'
+    fps.SetLED(True)
+    FPS.delay(delayTimer)
+    cycleUntilPressed(fps)
+    if fps.CaptureFinger() == False:
+            print 'Something went wrong while capturing your fingerprint'
+            return False
+    FPS.delay(delayTimer)
+    id = fps.Identify1_N()
+    if id == 200:
+        print 'No match found'
+    else:
+        print 'Fingerprint matched with id: ' + str(id)
+    FPS.delay(delayTimer)
+    fps.SetLED(False)
+    FPS.delay(delayTimer)
+    
+    
+        
+if __name__ == '__main__':
+    fps = FPS.FPS_GT511C3(device_name='/dev/ttyAMA0',baud=9600,timeout=2,is_com=False)
+    fps.UseSerialDebug = False
+    enrollFingerprint(fps)
+##    fingerprintID(fps)
+##    if fps.CheckEnrolled(0) == True:
+##        print '0 is enrolled'
+##    FPS.delay(0.5)
+##    if fps.CheckEnrolled(69) == True:
+##        print '69 is enrolled'
+##    FPS.delay(0.5)
+##    if fps.CheckEnrolled(100) == True:
+##        print '100 is enrolled'
+##    FPS.delay(0.5)
+##    if fps.CheckEnrolled(57) == True:
+##        print '57 is enrolled'
+##    FPS.delay(0.5)
+##    print 'Enrolled fingerprints: ' + str(fps.GetEnrollCount())
+    FPS.delay(0.5)
+    fps.Close()
