bd8b4c3cbb572a4ef795ca88feadce924ed2e19b
Cristian Marculescu <marculescucristian94@gmail.com>
2017-04-08 12:13:03 +0000

    Server ticket system v0

--- sockets/server.py
+++ sockets/server.py
@@ -2,14 +2,22 @@
 
 import socket               # Import socket module
 
+queue_dict = {}
+current_ticket = 0
+most_recent = 0
+
 s = socket.socket()         # Create a socket object
-host = '172.19.7.74' # Get local machine name
+host = '192.168.1.110'      # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
+   if not addr in queue_dict:
+      queue_dict[addr] = most_recent
+      most_recent += 1
+   response = 'Current ticket: ' + str(current_ticket) + ', Your ticket: ' + str(queue_dict[addr])   
    print 'Got connection from', addr
-   c.send('Thank you for connecting')
+   c.send(response)
    c.close()                # Close the connection
