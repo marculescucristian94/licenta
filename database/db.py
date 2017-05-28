085b51fdb41d0bbd336aadc7ea2ee45ca966492f
Cristian Marculescu <marculescucristian94@gmail.com>
2017-05-28 14:19:48 +0300

    Db insert improvments, added select and delete options

--- database/db.py
+++ database/db.py
@@ -1,34 +1,58 @@
 import sqlite3
 
-DATABASE = 'fingerpi.db'
+DATABASE = 'rasprint.db'
 
+# obtains the values that are inserted into the database 
 def to_dict(data):
     ret_dict = {}
-    pairs = data.split(',')
+    pairs = data.split('|')
     for pair in pairs:
         key, value = pair.split(':')
         ret_dict[key] = value
     return ret_dict
 
+# inserts or updates data
 def db_insert(fingerprint_id, data):
     conn = sqlite3.connect(DATABASE)
-    print 'Database opened successfully'
-    conn.execute('''CREATE TABLE IF NOT EXISTS USER_DATA
-                (ID             INTEGER PRIMARY KEY  NOT NULL,
-                 FINGERPRINT_ID INTEGER              NOT NULL,
-                 FIELD_TYPE     TEXT                 NOT NULL,
-                 FIELD_VALUE    TEXT                 NOT NULL);''')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS user_data
+                (id          INTEGER PRIMARY KEY  NOT NULL,
+                 fp_id       INTEGER              NOT NULL,
+                 field_type  TEXT                 NOT NULL,
+                 field_value TEXT                 NOT NULL);''')
     data_dict = to_dict(data)
     for f_type, f_value in data_dict.iteritems():
-        command = 'INSERT INTO USER_DATA (FINGERPRINT_ID, FIELD_TYPE, FIELD_VALUE) \
+        c.execute('SELECT id FROM user_data WHERE fp_id = %d AND field_type = \'%s\'' % (fingerprint_id, f_type))
+        existing = c.fetchall()
+        if not existing:
+            command = 'INSERT INTO user_data (fp_id, field_type, field_value) \
                 VALUES (%d, \'%s\', \'%s\')' % (fingerprint_id, f_type, f_value)
-        conn.execute(command)
+        else:
+            command = 'UPDATE user_data SET field_value = \'%s\' WHERE id = %d' % (f_value, existing[0][0])
+        c.execute(command)
+    conn.commit()
+    conn.close()
+
+# selects the relevant data for fingerprint_id
+def db_select(fingerprint_id):
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+    c.execute('SELECT field_type, field_value FROM user_data WHERE fp_id = %d' % (fingerprint_id))
+    result = c.fetchall()
+    print result
+    conn.close()
+
+# wipes all data for fingerprint_id
+def db_delete(fingerprint_id):
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+    c.execute('DELETE FROM user_data WHERE fp_id = %d' % (fingerprint_id))
     conn.commit()
     conn.close()
         
-        
-        
 
 if __name__ == '__main__':
-    db_insert(1, 'type1:value1,type2:value2')
+    db_insert(1, 'name:cristian|surname:marculescu')
+    db_select(1)
+    db_delete(1)
         
