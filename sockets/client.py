58e3d07b0a155dc6ddba7b24691c8e91734ed04b
Cristian Marculescu <marculescucristian94@gmail.com>
2017-05-30 17:16:50 +0300

    Finally working on sockets...

--- sockets/client.py
+++ sockets/client.py
@@ -4,16 +4,17 @@ import socket
 
 s = socket.socket()
 # Get IP address of fingerpi, works on Linux
-# host_port = socket.getaddrinfo('fingerpi.local', 12345)[0][4]
+host_port = socket.getaddrinfo('fingerpi.local', 12345)[0][4]
+print host_port
 
 # Get IP address of fingerpi, works on Windows
-host = '192.168.1.110'
-port = 12345
+#host = 'localhost'
+#port = 12345
 
 # Connect from Linux client
-# s.connect(host_port)
+s.connect(host_port)
 
 # Connect from Windows client
-s.connect((host, port))
+#s.connect((host, port))
 print s.recv(1024)
 s.close
--- sockets/server.py
+++ sockets/server.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python           # This is server.py file
 
 import socket               # Import socket module
+import time
 
 queue_dict = {}
-current_ticket = 0
-most_recent = 0
+#current_ticket = 0
+#most_recent = 0
 
 s = socket.socket()         # Create a socket object
-host = '192.168.1.110'      # Get local machine name
+host = 'localhost'          # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
+pending_clients = []
 
 s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
+   '''
    if not addr[0] in queue_dict:
       queue_dict[addr[0]] = most_recent
       most_recent += 1
    print queue_dict
-   response = 'Current ticket: ' + str(current_ticket) + ', Your ticket: ' + str(queue_dict[addr[0]])   
+   '''
+   if not addr[0] in pending_clients:
+      pending_clients.append(addr[0])
+   print pending_clients
+   if pending_clients[0] == addr[0]:
+      response = 'ACK'
+   else:
+      response = 'NACK'
+   #response = 'Current ticket: ' + str(current_ticket) + ', Your ticket: ' + str(queue_dict[addr[0]])   
    print 'Got connection from', addr
    c.send(response)
    c.close()                # Close the connection
